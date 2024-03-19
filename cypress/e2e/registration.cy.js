9f8fbf9d9dc108201f71c148d693612be74851c7
Your Name <your_email@whatever.com>
2024-03-19 21:16:54 +0700

    Регистрация с неверым отчеством

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -23,7 +23,6 @@ describe('Registration Test', () => {
         })
     })
 
-
     it('None existent login registration test', () => {
         cy.fixture('register').then(data => {
             cy.log('Переход на страницу регистрации')
@@ -159,6 +158,42 @@ describe('Registration Test', () => {
             cy.log('Непоходящее имя')
             cy.get('div[class="form__labels"] div:nth-child(2) div:nth-child(2) div input').type(data.none_existent_name)
 
+            cy.wait(1000)
+
+            cy.log('Вывод ошибки')
+            cy.get('div[class="form-error form-error-- form-error-- form-error--"]').should('exist')
+
+        })
+    })
+
+    it('None existent confirm patronymic registration test', () => {
+        cy.fixture('register').then(data => {
+            cy.log('Переход на страницу регистрации')
+            cy.visit(data.main_url)
+
+            cy.log('Подходящий логин')
+            cy.get('div[class="form__labels"] input:first').type(data.login)
+
+            cy.log('Подходящая почта')
+            cy.get('div[class="form__labels"] div:first div:nth-child(2) input').type(data.email)
+
+            cy.log('Подходящий пароль')
+            cy.get('div[class="form__labels"] div:first div:nth-child(3) input').type(data.password)
+
+            cy.log('Верный повтор пароля')
+            cy.get('div[class="form__labels"] div:first div:nth-child(4) input').type(data.confirm_password)
+
+            cy.log('Клик по кнопке "Далее"')
+            cy.get('div[class="form__buttons"] div:last button[type="submit"]').click()
+
+            cy.log('Походящяя фамилия')
+            cy.get('div[class="form__labels"] div:nth-child(2) div:nth-child(1) div input').type(data.surname)
+
+            cy.log('Походящее имя')
+            cy.get('div[class="form__labels"] div:nth-child(2) div:nth-child(2) div input').type(data.name)
+
+            cy.log('Неподходящее отчество')
+            cy.get('div[class="form__labels"] div:nth-child(2) div:nth-child(3) div input').type(data.none_existent_patronymic)
 
             cy.wait(1000)
 
