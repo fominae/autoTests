f3f64864d70eae9c0e1562f34046af8fe73d1fa1
Your Name <your_email@whatever.com>
2024-03-19 22:21:11 +0700

    Регистрация с верными данными

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,28 +1,4 @@
 describe('Registration Test', () => {
-    it('Existent registration test', () => {
-        cy.fixture('register').then(data => {
-            cy.log('Переход на страницу регистрации')
-            cy.visit(data.main_url)
-
-            cy.log('Подходящий логин')
-            cy.get('div[class="form__labels"] input:first').type(data.login)
-
-            cy.log('Подходящая почта')
-            cy.get('div[class="form__labels"] div:first div:nth-child(2) input').type(data.email)
-
-            cy.log('Подходящий пароль')
-            cy.get('div[class="form__labels"] div:first div:nth-child(3) input').type(data.password)
-
-            cy.log('Верный повтор пароля')
-            cy.get('div[class="form__labels"] div:first div:nth-child(4) input').type(data.confirm_password)
-
-            cy.log('Клик по кнопке "Далее"')
-            cy.get('div[class="form__buttons"] div:last button[type="submit"]').click()
-
-
-        })
-    })
-
     it('None existent login registration test', () => {
         cy.fixture('register').then(data => {
             cy.log('Переход на страницу регистрации')
@@ -53,6 +29,7 @@ describe('Registration Test', () => {
 
             cy.log('Вывод ошибки')
             cy.get('div[class="form-error form-error-- form-error-- form-error--"]').should('exist')
+
         })
     })
 
@@ -199,6 +176,41 @@ describe('Registration Test', () => {
 
             cy.log('Вывод ошибки')
             cy.get('div[class="form-error form-error-- form-error-- form-error--"]').should('exist')
+
+        })
+    })
+
+    it('Existent registration test', () => {
+        cy.fixture('register').then(data => {
+            cy.log('Переход на страницу регистрации')
+            cy.visit(data.main_url)
+
+            cy.log('Подходящий логин')
+            cy.get('div[class="form__labels"] input:first').type(data.login)
+
+            cy.log('Подходящая почта')
+            cy.get('div[class="form__labels"] div:first div:nth-child(2) input').type(data.email)
+
+            cy.log('Подходящий пароль')
+            cy.get('div[class="form__labels"] div:first div:nth-child(3) input').type(data.password)
+
+            cy.log('Верный повтор пароля')
+            cy.get('div[class="form__labels"] div:first div:nth-child(4) input').type(data.confirm_password)
+
+            cy.log('Клик по кнопке "Далее"')
+            cy.get('div[class="form__buttons"] div:last button[type="submit"]').click()
+
+            cy.log('Походящяя фамилия')
+            cy.get('div[class="form__labels"] div:nth-child(2) div:nth-child(1) div input').type(data.surname)
+
+            cy.log('Походящее имя')
+            cy.get('div[class="form__labels"] div:nth-child(2) div:nth-child(2) div input').type(data.name)
+
+            cy.log('Походящее отчество')
+            cy.get('div[class="form__labels"] div:nth-child(2) div:nth-child(3) div input').type(data.patronymic)
+
+            cy.log('Клик по кнопке "Создать аккаунт"')
+            cy.get('div[class="form__buttons"] div:nth-child(3) button[type="submit"]').click()
 
         })
     })
