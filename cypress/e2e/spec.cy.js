9edbc1e7ba1c10d0373fbd551e0f7080a432f151
Your Name <your_email@whatever.com>
2024-03-19 15:25:26 +0700

    Авторизация с существующими данными

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,18 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Login Test', () => {
+  it('Existent login test', () => {
+    cy.fixture('login_test').then(data => {
+      cy.log('Переход на страницу авторизации')
+      cy.visit(data.main_url)
+
+      cy.log('Существующий логин')
+      cy.get('input[class="form-input--text form-input"]').type(data.login)
+
+      cy.log('Существующий пароль')
+      cy.get('input[class="form-input--password form-input"]').type(data.password)
+
+      cy.log('Клик по кнопке "Войти"')
+      cy.get('div[class="login-form__button"] button[type="submit"]').click()
+
+    })
   })
 })
