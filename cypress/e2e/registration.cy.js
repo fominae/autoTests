a8783c6649cb9fd93c8dc1727962922e13d67aea
Your Name <your_email@whatever.com>
2024-03-19 19:38:53 +0700

    Регистрация с неверным паролем

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -54,4 +54,25 @@ describe('Registration Test', () => {
             cy.get('div[class="form-error form-error-- form-error-- form-error--"]').should('exist')
         })
     })
+
+    it('None existent password registration test', () => {
+        cy.fixture('register').then(data => {
+            cy.log('Переход на страницу регистрации')
+            cy.visit(data.main_url)
+
+            cy.log('Подходящий логин')
+            cy.get('div[class="form__labels"] input:first').type(data.login)
+
+            cy.log('Подходящая почта')
+            cy.get('div[class="form__labels"] div:first div:nth-child(2) input').type(data.email)
+
+            cy.log('Неподходящий пароль')
+            cy.get('div[class="form__labels"] div:first div:nth-child(3) input').type(data.none_existent_password)
+
+            cy.wait(1000)
+
+            cy.log('Вывод ошибки')
+            cy.get('div[class="form-error form-error-- form-error-- form-error--"]').should('exist')
+        })
+    })
 })
