00972b6372d78bff6e3340eba75e5e419aa054bc
Your Name <your_email@whatever.com>
2024-03-20 01:02:16 +0700

    Создание потребности с пустым полем с названием потребности

--- cypress/e2e/requirement.cy.js
+++ cypress/e2e/requirement.cy.js
@@ -80,4 +80,42 @@ describe('Requirement Test', () => {
         })
     })
 
+    it('Test requirement negative name', () => {
+        cy.fixture('requirement').then(data => {
+            cy.log('Переход на страницу авторизации')
+            cy.visit(data.main_url)
+
+            cy.log('Существующий логин')
+            cy.get('input[class="form-input--text form-input"]').type(data.login)
+
+            cy.log('Существующий пароль')
+            cy.get('input[class="form-input--password form-input"]').type(data.password)
+
+            cy.log('Клик по кнопке "Войти"')
+            cy.get('.button__background-color-green').click()
+
+            cy.log('Клик по ссылку "Потребности"')
+            cy.get('div[class="page-navigation"] div:first div:nth-child(6) p').click()
+
+            cy.log('Клик по ссылку "Создать потребность"')
+            cy.get('div[class="needs-block__content"] div:first button[type="submit"]').click()
+
+            cy.log('Подходящее название потребности')
+            cy.get('.desktop-modal .labels .label:first div input').type(data.name)
+            cy.get('.desktop-modal .labels .label:first div input').clear()
+
+            cy.wait(1000)
+
+            cy.log('Вывод ошибки')
+            cy.get('.desktop-modal .labels .label:first div:nth-child(2)').should('exist')
+
+            cy.log('Ввод обязанности')
+            cy.get('.desktop-modal .labels .label:nth-child(3) div textarea').type(data.responsibilities)
+
+            cy.log('Ввод требований')
+            cy.get('.desktop-modal .labels .label:nth-child(4) div textarea').type(data.requirements)
+
+        })
+    })
+
 })
